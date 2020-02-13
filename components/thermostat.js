4d50412e0779c73be0ad66851d327af2533c61b6
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-02-13 01:24:11 -0800

    Added unit changer

--- components/thermostat.js
+++ components/thermostat.js
@@ -10,7 +10,7 @@ class TemperatureDisplay extends React.Component {
 
     convertUnits(unit, temp){
         if(unit == 'C'){
-            return (temp - 32) * (5 / 9);
+            return (temp - 32) * (5 / 9).toFixed(1);
         }
         else{
             return temp
@@ -55,19 +55,35 @@ class TemperatureDisplay extends React.Component {
         this.tempDown(temp)
     }
 
+    changeUnits(unit){
+        if(unit == 'F'){
+            this.setState({tempUnit: 'C'});
+        }
+
+        if(unit == 'C'){
+            this.setState({tempUnit: 'F'});
+        }
+    }
+
     render() {
       return (
         <div>
             <h1>this is the temp</h1>
-            <h2>{this.convertUnits(this.state.tempUnit, this.state.temperature)}
-            °{this.state.tempUnit}</h2>
             <div>
-                <button disabled = {this.state.disUp} id = "up" class="btn btn-dark" onClick={() => this.disableUp(this.state.temperature)} > 
-                    <span class = "fas fa-arrow-up"></span>
+                <h2>{this.convertUnits(this.state.tempUnit, this.state.temperature)}
+                °{this.state.tempUnit}</h2>
+                <button disabled = {this.state.disUp} id = "up" className="btn btn-dark" onClick={() => this.disableUp(this.state.temperature)} > 
+                    <span className = "fas fa-arrow-up"></span>
                 </button>
-                <button disabled = {this.state.disDown} id = "down" class="btn btn-dark" onClick={() => this.disableDown(this.state.temperature)}>
-                    <span class = "fas fa-arrow-down"></span>
+                <button disabled = {this.state.disDown} id = "down" className="btn btn-dark" onClick={() => this.disableDown(this.state.temperature)}>
+                    <span className = "fas fa-arrow-down"></span>
                 </button>
+            </div>
+            <div className='custom-control custom-switch'>
+                <input type='checkbox' className='custom-control-input'id='customSwitches' readOnly/>
+                <label className='custom-control-label' htmlFor='customSwitches' onClick={() => this.changeUnits(this.state.tempUnit)}>
+                switch units
+                </label>
             </div>
         </div>
       );
