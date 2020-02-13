46b61f3cc444b9c660f52c3676f2e0079d76a10b
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-02-13 00:57:43 -0800

    Update thermostat.js

--- components/thermostat.js
+++ components/thermostat.js
@@ -2,7 +2,9 @@ class TemperatureDisplay extends React.Component {
     constructor(props) {
       super(props);
       this.state = {temperature: 68,
-                    tempUnit: 'F'
+                    tempUnit: 'F',
+                    disUp: false,
+                    disDown: false
         };
     }
 
@@ -15,12 +17,50 @@ class TemperatureDisplay extends React.Component {
         }
     }
 
+    tempUp(temp){
+        this.setState({temperature: temp + 1});
+    }
+
+    tempDown(temp){
+        this.setState({temperature: temp - 1});
+    }
+
+    disableUp(temp){
+        if(temp == 79){
+            document.getElementById("up").classList.add("disabled");
+            this.setState({disUp: true});
+        }
+        if(temp >= 50){
+            document.getElementById("down").classList.remove("disabled");
+            this.setState({disDown: false});
+        }
+    }
+
+    disableDown(temp){
+        if(temp == 51){
+            document.getElementById("down").classList.add("disabled");
+            this.setState({disDown: true});
+        }
+        if(temp <= 80){
+            document.getElementById("up").classList.remove("disabled");
+            this.setState({disUp: false});
+        }
+    }
+
     render() {
       return (
         <div>
             <h1>this is the temp</h1>
             <h2>{this.convertUnits(this.state.tempUnit, this.state.temperature)}
             °{this.state.tempUnit}</h2>
+            <div>
+                <button disabled = {!this.state.disUp} id = "up" class="btn btn-dark" onClick={() => this.disableUp(this.state.temperature)} > 
+                    <span class = "fas fa-arrow-up"></span>
+                </button>
+                <button disabled = {!this.state.disDown} id = "down" class="btn btn-dark" onClick={() => this.disableDown(this.state.temperature)}>
+                    <span class = "fas fa-arrow-down"></span>
+                </button>
+            </div>
         </div>
       );
     }
